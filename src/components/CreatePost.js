ab3ca33139ad3aca75ade371213a2f161dae2fbc
54chair <takukoshi3721@gmail.com>
2023-10-17 18:04:53 +0900

    記事一覧の実装

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import "./CreatePost.css"
 import { addDoc, collection } from "firebase/firestore"
 import { db, auth } from '../firebase';
 import { useNavigate } from "react-router-dom";
 
-const CreatePost = () => {
+const CreatePost = ({ isAuth }) => {
   const [title, setTitle] = useState();
   const [postText, setPostText] = useState();
   const navigate = useNavigate();
@@ -20,6 +20,12 @@ const CreatePost = () => {
     })
     navigate("/");
   };
+
+  useEffect(() => {
+    if(!isAuth) {
+      navigate("/login")
+    }
+  }, [])
 
   return (
     <div className="createPostPage">
